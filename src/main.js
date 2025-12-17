1580e1d6f45845c6379c6e458f0508f7cd18b245
Gerald Benischke <11561320+beny23@users.noreply.github.com>
2025-12-17 10:33:14 +0000

    Fit game to mobile screen

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import { GameScene } from './scenes/GameScene.js';
 const config = {
   type: Phaser.AUTO,
   parent: 'app',
-  width: 960,
-  height: 540,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 960,
+    height: 540
+  },
   backgroundColor: '#062033',
   physics: {
     default: 'arcade',
